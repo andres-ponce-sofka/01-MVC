9de4501d7c2dab1e2cf68cbe87fe5c58429c8b56
andres-ponce-sofka <andres.ponce@sofka.com.co>
2024-10-23 09:21:38 -0500

    init: springboot project init

---
+++ src/main/java/co/com/sofka/sofkau/cuentaflex/CuentaflexApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.sofkau.cuentaflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaflexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/sofkau/cuentaflex/CuentaflexApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.sofkau.cuentaflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
